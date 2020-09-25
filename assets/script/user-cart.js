9e28148040d98bbc6a707c1b9f434e2ea5308c2d
BhagiaSheri <bhagiasheri24@gmail.com>
2020-09-26 00:41:58 +0500

    feat: similar-produts-styling and logic updation

--- assets/script/user-cart.js
+++ assets/script/user-cart.js
@@ -125,7 +125,7 @@ function userCartProducts() {
 
                     // update the products data in localStorage with all details and total price
                     localStorage.setItem(productKey, JSON.stringify(productJson));
-                    setTimeout(location.reload(), 1000);
+                    setTimeout(location.reload(), 500);
                 }
             }
         });
